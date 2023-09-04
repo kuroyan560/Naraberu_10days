dce80734dd16d943c66a45ef8b89161588dffc38
K-rin <kingukarin@gmail.com>
2023-09-04 11:58:18 +0900

    imguiでゲームシーンデバッグ表示テスト

--- Naraberu_10days/Game/src/GameScene.cpp
+++ Naraberu_10days/Game/src/GameScene.cpp
@@ -1,4 +1,5 @@
 #include "GameScene.h"
+#include"FrameWork/ImguiApp.h"
 
 
 GameScene::GameScene()
@@ -19,6 +20,9 @@ void GameScene::OnDraw()
 
 void GameScene::OnImguiDebug()
 {
+	ImGui::Begin("GameScene");
+	ImGui::Text("Naw scene is GameScene.");
+	ImGui::End();
 }
 
 void GameScene::OnFinalize()
