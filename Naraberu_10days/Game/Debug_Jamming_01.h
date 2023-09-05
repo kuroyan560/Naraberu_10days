512830606dc0d2a8a03438b84c4de93f27423a1f
oka_tomohiro <k020g1216@g.neec.ac.jp>
2023-09-05 15:10:43 +0900

    競合直し
    ああ

--- Naraberu_10days/Game/Debug_Jamming_01.h
+++ Naraberu_10days/Game/Debug_Jamming_01.h
@@ -1,30 +1,30 @@
-#pragma once
-#include "E_Ac_Base.h"
-#include <memory>
-
-class UnitBase;
-
-class Debug_Heal_01 : public E_Ac_Base
-{
-
-private:
-	int m_Damage;
-
-	// --------
-	// Ž©•ª
-	UnitBase* m_pUnit;
-public:
-	Debug_Heal_01() {
-		m_Damage = 0;
-		m_pUnit = nullptr;
-	};
-	void Param_Set(int Damage, int Duration);
-	void Need_Object_Set(UnitBase* pUnit);
-
-public:
-	void Action_Start()override;
-	void Action_Update()override;
-	void Action_Draw()override;
-	void Action_End()override;
-};
-
+//#pragma once
+//#include "E_Ac_Base.h"
+//#include <memory>
+//
+//class UnitBase;
+//
+//class Debug_Heal_01 : public E_Ac_Base
+//{
+//
+//private:
+//	int m_Damage;
+//
+//	// --------
+//	// Ž©•ª
+//	UnitBase* m_pUnit;
+//public:
+//	Debug_Heal_01() {
+//		m_Damage = 0;
+//		m_pUnit = nullptr;
+//	};
+//	void Param_Set(int Damage, int Duration);
+//	void Need_Object_Set(UnitBase* pUnit);
+//
+//public:
+//	void Action_Start()override;
+//	void Action_Update()override;
+//	void Action_Draw()override;
+//	void Action_End()override;
+//};
+//
