b847c5bcb499a86894e5830cdb8431d4710785ba
oka_tomohiro <k020g1216@g.neec.ac.jp>
2023-09-07 14:17:55 +0900

    h戻し
    あ

--- Naraberu_10days/Game/src/Scene/TitleScene.cpp
+++ Naraberu_10days/Game/src/Scene/TitleScene.cpp
@@ -1,8 +1,22 @@
 #include "TitleScene.h"
 #include "../OperationConfig.h"
+#include "../../src/engine/DirectX12/D3D12App.h"
 
 void TitleScene::OnInitialize()
 {
+	using namespace KuroEngine;
+	std::string TexDir = "resource/user/tex/select_scene/";
+
+	// ”wŒi
+	backTex = D3D12App::Instance()->GenerateTextureBuffer(TexDir + "select_scene_sukasi.png");
+
+	//ƒ^ƒCƒgƒ‹
+	titleTex = D3D12App::Instance()->GenerateTextureBuffer(TexDir + "select_scene_sukasi.png");
+	//‘I‘ðŽˆ
+	stageTex = D3D12App::Instance()->GenerateTextureBuffer(TexDir + "stage.png");
+	//”Žš
+	KuroEngine::D3D12App::Instance()->GenerateTextureBuffer(numTex.data(), TexDir + "pass_number.png", 3, { 3, 1 });
+
 }
 
 void TitleScene::OnUpdate()
--- Naraberu_10days/Game/src/Scene/TitleScene.h
+++ Naraberu_10days/Game/src/Scene/TitleScene.h
@@ -3,6 +3,19 @@
 
 class TitleScene : public KuroEngine::BaseScene
 {
+	// 背景
+	std::shared_ptr<KuroEngine::TextureBuffer> backTex;
+	//タイトル
+	std::shared_ptr<KuroEngine::TextureBuffer> titleTex;
+	//選択肢
+	std::shared_ptr<KuroEngine::TextureBuffer> stageTex;
+	//数字
+	std::array<std::shared_ptr<KuroEngine::TextureBuffer>,10> numTex;
+
+	//選択肢番号
+	int stageNum;
+
+
 	void OnInitialize()override;
 	void OnUpdate()override;
 	void OnDraw()override;
